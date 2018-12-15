8c98cbfa6f7686f9baa41a8e5042c25d24607d2f
Chris Boesch <ChrisBoesch@nus.edu.sg>
2018-12-15 17:50:01 +0800

    Updating react project to have a single failing test.

--- react/src/index.js
+++ react/src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
 exports.hello = function() {
-  return "Hi";
+  return "Hello";
 };
 
 function App() {
--- react/src/index.spec.js
+++ react/src/index.spec.js
@@ -1,9 +1,6 @@
 let greetings = require("./index");
 
 describe("Greeting", () => {
-  it("knows that hello returns HELLO", () => {
-    expect(greetings.hello()).toBe("HELLO");
-  });
   it("knows that hello returns Hi", () => {
     expect(greetings.hello()).toBe("Hi");
   });
