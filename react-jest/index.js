1d9e68e4f5dd5c190f168ad0ad8d1b3206ffcddc
Chris Boesch <ChrisBoesch@nus.edu.sg>
2018-12-15 20:30:39 +0800

    Failing test: Default Button count should be 0.

--- react-jest/index.js
+++ react-jest/index.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
 
 export default class Button extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: props.count
+    if (!props.count) {
+      this.state = {
+        count: 1
+      };
+    } else {
+      this.state = {
+        count: props.count
+      };
     }
+
     this.plusOne = this.plusOne.bind(this);
   }
 
   plusOne() {
-    this.setState({count: this.state.count +1});
+    this.setState({ count: parseInt(this.state.count) + 1 });
   }
 
   render() {
-    return (
-      <button onClick={this.plusOne}>{this.state.count}</button>
-    );
+    return <button onClick={this.plusOne}>{this.state.count}</button>;
   }
 }
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Button count="0" />, rootElement);
--- react-jest/index.spec.js
+++ react-jest/index.spec.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-Enzyme.configure({adapter: new Adapter()});
+Enzyme.configure({ adapter: new Adapter() });
 
-import Button from './index';
+import Button from "./index";
 
-it('Button text value after click', () => {
+it("Default Button count should be 0.", () => {
+  // Render a checkbox with label in the document
+  const buttonComponent = shallow(<Button />);
+
+  expect(buttonComponent.text()).toEqual("0");
+});
+
+it("Button text value after click", () => {
   // Render a checkbox with label in the document
   const buttonComponent = shallow(<Button count={1} />);
 
-  expect(buttonComponent.text()).toEqual('1');
+  expect(buttonComponent.text()).toEqual("1");
 
-  buttonComponent.find('button').simulate('click');
+  buttonComponent.find("button").simulate("click");
 
-  expect(buttonComponent.text()).toEqual('2');
+  expect(buttonComponent.text()).toEqual("2");
 });
