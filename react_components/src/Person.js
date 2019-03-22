320c23b4459ce58d0f8ad369d5bf060329c92194
Thangamanir <35291998+Thangamanir@users.noreply.github.com>
2019-03-22 15:52:35 +0800

    Added problem

--- react_components/src/Person.js
+++ react_components/src/Person.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 
 const Person = props => {
-  return <h3>I am a Person</h3>;
+  //Enter code for Person Component
+  //return <h3>I am a Person</h3>;
 };
 
 export default Person;
