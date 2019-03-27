776a46be121adcd7c2759c811b105fdeb689691b
thangamani <thangamani.r@gmail.com>
2019-03-27 09:51:13 +0800

    Updated test cases with react-test-renderer

--- react_props/src/index.spec.js
+++ react_props/src/index.spec.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Enzyme ,{ shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16.3';
+import {create} from "react-test-renderer";
 import Person from "./Person";
-Enzyme.configure({ adapter: new Adapter() });
+
 describe("The Person component", () => {
   it("Testing Person Component", () => {
-    const wrapper = shallow(<Person name="Test" />);
-    const text_entry = wrapper.text();
-    expect(text_entry).toEqual("Hello Test");
+    const component = create(<Person/>);
+    expect(component.toJSON()).toMatchSnapshot();
   });
 });
